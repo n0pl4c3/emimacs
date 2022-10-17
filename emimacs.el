752bf3a21efb0e630bc7e929ff85f8669e5af1c3
n0pl4c3 <69087176+n0pl4c3@users.noreply.github.com>
2022-10-17 18:53:01 +0200

    Add company autocomplete shortcut

--- emimacs.el
+++ emimacs.el
@@ -285,13 +285,14 @@
   ;; (company-begin-commands nil) ;; uncomment to disable popup
   :bind
   (:map company-active-map
-	      ("C-n". company-select-next)
-	      ("C-p". company-select-previous)
-	      ("M-<". company-select-first)
-	      ("M->". company-select-last)
-  (:map company-mode-map
-              ("<tab>". tab-indent-or-complete)
-	      ("TAB". tab-indent-or-complete))))
+              ("C-n". company-select-next)
+              ("C-p". company-select-previous)
+              ("M-<". company-select-first)
+              ("M->". company-select-last) 
+   (:map company-mode-map
+               ("<tab>". tab-indent-or-complete)
+               ("TAB". tab-indent-or-complete)
+               ("M-.".'company-complete))))
 
 (use-package yasnippet
   :ensure
